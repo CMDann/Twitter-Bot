7e3c67febb2dab0e366786f9bd09ea83688ce7d2
Daniel Blair <CMDann.Development@gmail.com>
2013-10-03 13:37:50 -0500

    Update twit_bot.py
    fixed some indenting

--- twit_bot.py
+++ twit_bot.py
@@ -68,149 +68,149 @@ class PostScheduler(threading.Thread):
                 return False
 
 class PostController(object):
-def __init__(self, post_composers = [], postControllers = None, current_user=None):
-    self.post_composers = post_composers
-    self.current_user = current_user
+    def __init__(self, post_composers = [], postControllers = None, current_user=None):
+        self.post_composers = post_composers
+        self.current_user = current_user
 
-def can_handle_object(self, post_object):
-    return len(post_object) == 0
+    def can_handle_object(self, post_object):
+        return len(post_object) == 0
 
-def probabilityToPost(self, post_object, seconds_from_midnight, time_step, simulate=False):
-    if len(post_object) != 0:
-        return 0
-    if self.isCurrentUser(post_object):
-        return 0
-    # flat distribution 22 tweets per day
-    one_day = 60.*60.*24./float(time_step)
-    if simulate:
-        one_day /= 60
-    return 22./one_day
+    def probabilityToPost(self, post_object, seconds_from_midnight, time_step, simulate=False):
+        if len(post_object) != 0:
+            return 0
+        if self.isCurrentUser(post_object):
+            return 0
+        # flat distribution 22 tweets per day
+        one_day = 60.*60.*24./float(time_step)
+        if simulate:
+            one_day /= 60
+        return 22./one_day
 
-def isCurrentUser(self, post_object):
-    if self.current_user == None:
-        print 'No current user skipping'
+    def isCurrentUser(self, post_object):
+        if self.current_user == None:
+            print 'No current user skipping'
+            return False
+        # don't respond if the tweet belongs to the current user -- would be infinite loop!
+        if post_object.has_key('user'):
+            if post_object['user'].has_key('id_str'):
+                return post_object['user']['id_str'] == self.current_user['id_str']
         return False
-    # don't respond if the tweet belongs to the current user -- would be infinite loop!
-    if post_object.has_key('user'):
-        if post_object['user'].has_key('id_str'):
-            return post_object['user']['id_str'] == self.current_user['id_str']
-    return False
 
-def choosePostComposer(self):
-    post_composers = []
-    total_percent = 0
-    for post_composer in self.post_composers:
-        if post_composer.percent() == 100:
-            return post_composer
-        post_composers.append(post_composer)
-        total_percent += post_composer.percent()
-    probability = random.randrange(total_percent)
-    threshold = 0
-    for post_composer in post_composers:
-        if threshold <= post_composer.percent():
-            return post_composer
-        threshold += post_composer.percent()
-    return post_composer
+    def choosePostComposer(self):
+        post_composers = []
+        total_percent = 0
+        for post_composer in self.post_composers:
+            if post_composer.percent() == 100:
+                return post_composer
+            post_composers.append(post_composer)
+            total_percent += post_composer.percent()
+        probability = random.randrange(total_percent)
+        threshold = 0
+        for post_composer in post_composers:
+            if threshold <= post_composer.percent():
+                return post_composer
+            threshold += post_composer.percent()
+        return post_composer
 
-def composePost(self, api, post_object, simulate):
-    return self.choosePostComposer().compose(api, post_object, simulate)
+    def composePost(self, api, post_object, simulate):
+        return self.choosePostComposer().compose(api, post_object, simulate)
 
-class ReplyController(post.PostController):
-def __init__(self, post_composers = [], postControllers = None, current_user=None):
-    post.PostController(post_composers, postControllers, current_user)
-    self.post_composers = post_composers
-    self.current_user = current_user
-    self.reply_ids = { }
+    class ReplyController(post.PostController):
+    def __init__(self, post_composers = [], postControllers = None, current_user=None):
+        post.PostController(post_composers, postControllers, current_user)
+        self.post_composers = post_composers
+        self.current_user = current_user
+        self.reply_ids = { }
 
-def can_handle_object(self, post_object):
-    if self.isCurrentUser(post_object):
+    def can_handle_object(self, post_object):
+        if self.isCurrentUser(post_object):
+            return False
+        if not post_object.has_key('entities'):
+            return False
+        if not post_object['entities'].has_key('user_mentions'):
+            return False
+        for user_mention in post_object['entities']['user_mentions']:
+            if user_mention['id_str'] == self.current_user['id_str']:
+                return True
         return False
-    if not post_object.has_key('entities'):
-        return False
-    if not post_object['entities'].has_key('user_mentions'):
-        return False
-    for user_mention in post_object['entities']['user_mentions']:
-        if user_mention['id_str'] == self.current_user['id_str']:
-            return True
-    return False
 
-def probabilityToPost(self, post_object, seconds_from_midnight, time_step, simulate=False):
-    if self.isCurrentUser(post_object):
+    def probabilityToPost(self, post_object, seconds_from_midnight, time_step, simulate=False):
+        if self.isCurrentUser(post_object):
+            return 0
+        if not post_object.has_key('entities'):
+            return 0
+        if not post_object['entities'].has_key('user_mentions'):
+            return 0
+        for user_mention in post_object['entities']['user_mentions']:
+            if user_mention['id_str'] == self.current_user['id_str']:
+                return self.probabilityForId(post_object, seconds_from_midnight, time_step)
         return 0
-    if not post_object.has_key('entities'):
-        return 0
-    if not post_object['entities'].has_key('user_mentions'):
-        return 0
-    for user_mention in post_object['entities']['user_mentions']:
-        if user_mention['id_str'] == self.current_user['id_str']:
-            return self.probabilityForId(post_object, seconds_from_midnight, time_step)
-    return 0
 
-def probabilityForId(self, post_object, seconds_from_midnight, time_step):
-    if not post_object.has_key('user'):
-        return 0
-    if not post_object['user'].has_key('id_str'):
-        return 0
-    user_id = post_object['user']['id_str']
-    if not self.reply_ids.has_key(user_id):
-        self.reply_ids[user_id] = { 'probability' : 1, 'first_reply' : datetime.today(), 'last_attempt' : datetime.min }
+    def probabilityForId(self, post_object, seconds_from_midnight, time_step):
+        if not post_object.has_key('user'):
+            return 0
+        if not post_object['user'].has_key('id_str'):
+            return 0
+        user_id = post_object['user']['id_str']
+        if not self.reply_ids.has_key(user_id):
+            self.reply_ids[user_id] = { 'probability' : 1, 'first_reply' : datetime.today(), 'last_attempt' : datetime.min }
 
-    current_datetime = datetime.today()
-    if (current_datetime - self.reply_ids[user_id]['first_reply']).seconds > 1:#60*60*24:
-        self.reply_ids[user_id] = { 'probability' : 1, 'first_reply' : datetime.today(), 'last_attempt' : datetime.min }
-        return 1
+        current_datetime = datetime.today()
+        if (current_datetime - self.reply_ids[user_id]['first_reply']).seconds > 1:#60*60*24:
+            self.reply_ids[user_id] = { 'probability' : 1, 'first_reply' : datetime.today(), 'last_attempt' : datetime.min }
+            return 1
 
-    probability = self.reply_ids[user_id]['probability']
-    delta = (datetime.today() - self.reply_ids[user_id]['last_attempt'])
-    if delta.microseconds < 500:
-        probability = 0
+        probability = self.reply_ids[user_id]['probability']
+        delta = (datetime.today() - self.reply_ids[user_id]['last_attempt'])
+        if delta.microseconds < 500:
+            probability = 0
 
-    self.reply_ids[user_id]['last_attempt'] = datetime.today()
+        self.reply_ids[user_id]['last_attempt'] = datetime.today()
 
-    return probability
+        return probability
 
-def postUpdateStatus(self, api, post_object):
-    user_id = post_object['user']['id_str']
-    probability = float(self.reply_ids[user_id]['probability'])
-    self.reply_ids[user_id]['probability'] = probability * 0.5
+    def postUpdateStatus(self, api, post_object):
+        user_id = post_object['user']['id_str']
+        probability = float(self.reply_ids[user_id]['probability'])
+        self.reply_ids[user_id]['probability'] = probability * 0.5
 
-    class FortuneComposer(PostComposer):
-def __init__(self):
-    self.fortunes = open('fortunes').read().split('\n%\n')
-    for fortune in self.fortunes:
-        if len(fortune) > 140:
-            self.fortunes.remove(fortune)
+        class FortuneComposer(PostComposer):
+    def __init__(self):
+        self.fortunes = open('fortunes').read().split('\n%\n')
+        for fortune in self.fortunes:
+            if len(fortune) > 140:
+                self.fortunes.remove(fortune)
 
-def compose(self, api, post_object, simulate):
-    fortune = None
-    screen_name = None
-    if post_object.has_key('user'):
-        if post_object['user'].has_key('screen_name'):
-            screen_name = post_object['user']['screen_name']
-    if screen_name != None:
-        fortune = self.chooseFortune(140, screen_name)
-    else:
-        fortune = self.chooseFortune()
-    if fortune == None:
-        return None
-    if simulate:
-        return fortune
-    if post_object.has_key('id_str') and screen_name != None:
-        return api.updateStatus(status=fortune, in_reply_to_status_id=post_object['id_str'])
-    else:
-        return api.updateStatus(status=fortune)
-
-def chooseFortune(self, max_len=140, screen_name=None):
-    fortune = ''
-    if screen_name != None:
-        fortune += '@' + screen_name + ' '
-        max_len -= len(fortune)
-    tmp_fortune = random.choice(self.fortunes)
-    count = 0
-    while len(tmp_fortune) > max_len:
-        if count > 1000:
+    def compose(self, api, post_object, simulate):
+        fortune = None
+        screen_name = None
+        if post_object.has_key('user'):
+            if post_object['user'].has_key('screen_name'):
+                screen_name = post_object['user']['screen_name']
+        if screen_name != None:
+            fortune = self.chooseFortune(140, screen_name)
+        else:
+            fortune = self.chooseFortune()
+        if fortune == None:
             return None
+        if simulate:
+            return fortune
+        if post_object.has_key('id_str') and screen_name != None:
+            return api.updateStatus(status=fortune, in_reply_to_status_id=post_object['id_str'])
+        else:
+            return api.updateStatus(status=fortune)
+
+    def chooseFortune(self, max_len=140, screen_name=None):
+        fortune = ''
+        if screen_name != None:
+            fortune += '@' + screen_name + ' '
+            max_len -= len(fortune)
         tmp_fortune = random.choice(self.fortunes)
-        count += 1
-    fortune += tmp_fortune
-    return fortune
+        count = 0
+        while len(tmp_fortune) > max_len:
+            if count > 1000:
+                return None
+            tmp_fortune = random.choice(self.fortunes)
+            count += 1
+        fortune += tmp_fortune
+        return fortune
